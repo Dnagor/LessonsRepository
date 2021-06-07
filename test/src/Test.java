66c8634543a17f5f0d5bdbf33ea8775ff4b19952
dnagor <dnagor0@gmail.com>
2021-06-07 13:04:35 +0300

    JSON parsing simple +- tests

---
+++ test/src/Test.java
@@ -0,0 +1,63 @@
+package src;
+
+import com.jayway.jsonpath.JsonPath;
+import org.testng.Assert;
+
+
+public class Test {
+    String result,result2;
+    String expectedResult = "Test1";
+    String expectedResult2 = "DelphiN";
+
+    public void testData() {
+        String testJson = "{\n" +
+                "\t\"websites\": [\n" +
+                "\t\t{\n" +
+                "\t\t\t\"amazon\": {\n" +
+                "\t\t\t\t\"id\": 1,\n" +
+                "\t\t\t\t\"books\": [\n" +
+                "\t\t\t\t\t{\n" +
+                "\t\t\t\t\t\t\"author\": \"Test1\",\n" +
+                "\t\t\t\t\t\t\"bookTitle\": \"Pascal\"\n" +
+                "\t\t\t\t\t},\n" +
+                "\t\t\t\t\t{\n" +
+                "\t\t\t\t\t\t\"author\": \"Test2\",\n" +
+                "\t\t\t\t\t\t\"bookTitle\": \"JS\"\n" +
+                "\t\t\t\t\t}\n" +
+                "\t\t\t\t]\n" +
+                "\t\t\t}\n" +
+                "\t\t},\n" +
+                "\t\t{\n" +
+                "\t\t\t\"rozetka\": {\n" +
+                "\t\t\t\t\"id\": 2,\n" +
+                "\t\t\t\t\"books\": [\n" +
+                "\t\t\t\t\t{\n" +
+                "\t\t\t\t\t\t\"author\": \"Test3\",\n" +
+                "\t\t\t\t\t\t\"bookTitle\": \"java\"\n" +
+                "\t\t\t\t\t},\n" +
+                "\t\t\t\t\t{\n" +
+                "\t\t\t\t\t\t\"author\": \"Test4\",\n" +
+                "\t\t\t\t\t\t\"bookTitle\": \"Delphi\"\n" +
+                "\t\t\t\t\t}\n" +
+                "\t\t\t\t]\n" +
+                "\t\t\t}\n" +
+                "\t\t}\n" +
+                "\t]\n" +
+                "}";
+        result = JsonPath.read(testJson, "$.websites[0].amazon.books[0].author");
+        result2 = JsonPath.read(testJson,"$.websites[1].rozetka.books[1].bookTitle");
+        System.out.println(result);
+        System.out.println(result2);
+    }
+
+    @org.testng.annotations.Test
+    public void positiveTest() {
+        testData();
+        Assert.assertEquals(result, expectedResult);
+    }
+    @org.testng.annotations.Test
+    public void negativeTest(){
+        testData();
+        Assert.assertNotEquals(result2,expectedResult2);
+    }
+}
